c363e516d896d6944d02703fef36d4e1b9640598
xDopeHatex <xgorokhovx@gmail.com>
2022-09-02 12:35:51 +0400

    init commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Mulish', 'sans-serif'],
+        
+      },
+      colors: {
+        pink: {
+          
+          900: '#FF2E65', 
+        },
+      },
+    },
+  },
+  plugins: [],
+}
